5dd22d447653e078def8902fef41d0c82e291abe
darshanjag <itsdarshanjagtap@gmail.com>
2019-02-01 13:22:55 +0530

    day 2 loops

---
+++ loops.js
@@ -0,0 +1,18 @@
+
+function vowelsAndConsonants(s) {
+    var s="javascriptloops"
+    for (var i = 0; i < s.length; i++) { 
+        if ((s[i] == "a") || (s[i == "e"]) || (s[i] == "i") || (s[i] == "o") || (s[i] == "u")) {
+            console.log(s[i]);
+        }
+    }
+
+    for (var j = 0; j < s.length; j++) {
+        if
+        ((s[j] != "a") && (s[j] != "e") && (s[j] != "i") && (s[j] != "o") && (s[j] != "u")) { 
+            console.log(s[j])
+        }
+        
+    }
+}
+vowelsAndConsonants();
