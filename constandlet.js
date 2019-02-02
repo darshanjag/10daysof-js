317b0c83b745d924e2635cae15c053d0c1da145d
darshanjag <itsdarshanjagtap@gmail.com>
2019-02-02 15:29:02 +0530

    day 1 let and const

---
+++ constandlet.js
@@ -0,0 +1,11 @@
+    var r = readLine();
+
+const PI= Math.PI
+    
+    // Print the area of the circle:
+    var area = PI *( r*r);
+    console.log(area);
+    
+    // Print the perimeter of the circle:
+    var perimeter = 2 * PI * r;
+    console.log(perimeter);
